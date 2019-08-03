f0c8016e5878ccfc6726e37f02042abf3eb2fd3c
dhruv <dhruv.160410116084@gmail.com>
2019-08-03 14:38:20 +0530

    added primenumber validation with both way old school and optimized

---
+++ src/ds/algo/DsAlgo.java
@@ -0,0 +1,13 @@
+
+package ds.algo;
+
+public class DsAlgo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
