b9d07d17903845b4ff1390a03372342f6458c3ca
dvarc <dhruv.160410116084@gmail.com>
2020-01-05 12:08:24 +0530

    added randomnized version

--- OrderStatistics.java
+++ OrderStatistics.java
@@ -1,62 +1,73 @@
-import java.lang.Math;
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 class OrderStatistics{
-    
-        int getMinimum(int arr[]){  //O(n)
-            int min = arr[0];
-            for(int i=1;i<arr.length;i++){
-                if(arr[i]<min)
-                    min = arr[i];
+    int randomizedPartition(int arr[],int p,int r){
+        // System.out.println("p :"+p+"r :"+r);
+        int rand = ThreadLocalRandom.current().nextInt(p,r+1);
+         System.out.println("rand ="+rand+"   "+"p ="+p+"r ="+r);
+        arr[r] = arr[r]+arr[rand]-(arr[rand]=arr[r]);
+        return partition(arr,p,r);
+
+    }
+
+    int partition(int arr[],int p,int r){
+        int x = arr[r];
+        int i = p-1;
+        for(int j=p ;j<r;j++){
+            if(arr[j] <= x){
+                i++;
+                arr[i]=arr[i]+arr[j]-(arr[j]=arr[i]);
+
             }
-            return min;
         }
+        arr[i+1] = arr[i+1]+arr[r]-(arr[r]=arr[i+1]);
+        return i+1;
+    }
 
-        int getMaximum(int arr[]){  //O(n)
-            int max = arr[0];
-            for(int i =1;i<arr.length;i++)
-                if(arr[i]>max)
-                    max = arr[i];
-            return max;
+    int randomizedSelect(int arr[],int p,int r,int i){
+        // System.out.println("-----------------------");
+        if(p == r)
+            return arr[p];
+        int q=randomizedPartition(arr, p, r);
+        //  pirnt(arr,p,r);
+        int k=q-p+1;
+        if(i==k)
+            return arr[q];
+        else if(i < k)
+            return randomizedSelect(arr, p, q-1, i);
+        else 
+            return randomizedSelect(arr, q+1, r, i-k);
+
+    }
+
+    void pirnt(int arr[],int p,int r){
+        System.out.println();
+        for(int i=p;i<=r;i++){
+            System.out.print(" "+arr[i]);
         }
-
-        int[] getMinAndMax(int arr[]){
-            int min = arr[0];
-            int max = arr[0];
-            System.out.println(arr.length);
-            for(int i=1;i<(arr.length);i=i+2){
-                System.out.println(" i="+i+" arr["+i+"] ="+arr[i]+" arr["+(i+1)+"] ="+arr[i+1]);
-                if(arr[i] < arr[i+1]){
-                    if(arr[i] < min)
-                        min = arr[i];
-                    if(arr[i+1] > max)
-                        max = arr[i]+1;
-                    
-                }
-                if(arr[i] > arr[i+1]){
-                    if(arr[i] > max)
-                        max = arr[i];
-                    if(arr[i+1] < min)
-                        min = arr[i+1];
-                 }
-            }
-            int[] minMax = {min,max};
-            return  minMax;
-        }
-
-
+        System.out.println();
+    }
     
 
     public static void main(String[] args) {
         OrderStatistics o = new OrderStatistics();
-        int arr[] = {34,56,8,12,3,90,23,56,77,44,22};
-        int a[] = o.getMinAndMax(arr);
-        System.out.println("min : "+o.getMinimum(arr)+"  : "+o.getMaximum(arr)+"  both: "+a[0]+" "+a[1]);
+        int arr[] = {34,8,12,3,90,23,56,77,44,22};
         
+        for(int i=0;i<arr.length;i++){
+           
+            System.out.print(arr[i]+" ");
+        }
+        // Arrays.sort(arr);
+        // o.pirnt(arr,0,arr.length-1);
+        System.out.println();
+        // for(int i=0;i<arr.length;i++){
+            // System.out.println(" for "+i);
+            int ans = o.randomizedSelect(arr,0,arr.length-1,8);
+            System.out.print(ans+" ");
+        // }
+        System.out.println();
+       
     }
 
 }
