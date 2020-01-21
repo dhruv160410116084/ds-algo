3d52798741940ca770d9bdc571f418dd9b9b3843
dhruv <dhruv.160410116084@gmail.com>
2020-01-21 13:43:31 +0530

    update feature added

--- ST.java
+++ ST.java
@@ -20,6 +20,26 @@ class ST{
         return st[i];
     }
 
+    void updateSum(int arr[],int i,int updateVal){
+        int diff = updateVal - arr[i];    
+        arr[i] = updateVal;
+        updateSumUtil(arr, 0, arr.length-1, i,diff, 0);
+
+    }
+
+    private void updateSumUtil(int arr[],int start,int end,int index,int diff,int i){
+            if(index< start || index> end)
+                return;
+
+       st[i] = diff+st[i];
+       if(start != end){
+           int mid = (start+end)/2;
+           updateSumUtil(arr, start, mid,index, diff, 2*i+1);
+           updateSumUtil(arr, mid+1, end, index,diff, 2*i+2);
+       }
+    }
+
+
     int getSum(int arr[],int start,int end){
             if(start > end || end < start){
                 return 0;
@@ -39,10 +59,15 @@ class ST{
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
-        ST t = new ST(arr);
-        t.printST();
-        System.out.println("sum of range"+t.getSum(arr,2,4));
+       int  st = 2*(int)Math.pow(2,Math.ceil((int)Math.log(3)/Math.log(2)))-1;
+        System.out.println(st);
+        // int arr[] = {1,2,3,4,5,6};
+        // ST t = new ST(arr);
+        // t.printST();
+        // System.out.println("sum of range"+t.getSum(arr,1,3));
+        // t.updateSum(arr, 1,5);  
+        // t.printST();  
+        // System.out.println("sum of range"+t.getSum(arr,1,3));
     }
 
 }
